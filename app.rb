067083abef7f5268b48370ab76b7591af98f31cb
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-02-15 17:27:51 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/' do
+     puts params    
+     @piglatin = PigLatinizer.new(params[:prepig])
+     
+     binding.pry
+     erb :display_results
+    end
+    
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,75 @@
+class PigLatinizer
+    # all your ruby methods will be in here to PLatinize
+
+    attr_accessor :pl, #:one
+
+    #split into an array of strings, guillotine each string, then join back together
+
+    def initialize(pl)
+        # @one = one
+        pl.split().map do |one|
+            guillotine(one).join('')
+        end
+    end
+    
+    def guillotine
+        binding.pry
+        if vowel_start?(one)
+            one + "way"
+        else
+            consonant_shift(one)
+            if vowel_start?(one)
+                one + "ay"
+            else
+                guillotine
+            end
+        end
+    end
+    
+    def vowel_start?(one)
+        one[0].start_with?(/[aeiouAEIOU]/)
+    end
+    
+    def consonant_shift(one)
+        # binding.pry
+        one += one[0]
+        one = one[1..-1]
+    end
+end
+    
+    # def try_again(two)
+    #     consonant_shift(one)
+    #         if vowel_start?(one)
+    #             one + "ay"
+    #         else
+    #             try_again(one)
+    #         end        
+    # end
+    
+    # def pigmy_in_results
+    #     results_array << @letters.join
+    # end
+    
+#     def word_count #["Ryan Michael Bollettino"] #=> 3
+#         pl.split.count  
+#     end
+    
+#     @original.pl.each.to_a do 
+#     @letters = pl.split('')
+#     @letters.guillotine
+#     i=+1 until i < 
+
+# @original.pl.map do |x|
+#     @letters = x.split('')
+# end 
+# #=> [["R", "y", "a", "n"], ["M", "i", "c", "h", "a", "e", "l"], ["B", "o", "l", "l", "e", "t", "t", "i", "n", "o"]]
+
+
+#     # def separate
+#     #     arr1 = pl.split()
+#     # end
+
+#     # def letters(@arr1)     #["R", "y", "a", "n"]
+#     #     one_word.split('')
+#     #     pl.split[0].split('')
+#     # end
